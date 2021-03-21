705effb4894929330f17f8ccc17996cd648dc01c
Yavor Petkov <yavop.98@gmail.com>
2021-03-22 00:05:03 +0200

    Add very basic "Adding student functionality'

--- client/dom-example.js
+++ client/dom-example.js
@@ -35,4 +35,80 @@
     function deleteTableRow(deleteButtonElement) {
         deleteButtonElement.parentElement.parentElement.remove();
     }
+
+    const submitFormButton = document.getElementById('submit')
+    submitFormButton.addEventListener('click', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        const personToBeAdded = extractInputValues();
+        if(!personToBeAdded.firstName || personToBeAdded.lasttName 
+            || !personToBeAdded.mark || !personToBeAdded.fn) {
+                alert('Enter all of the fields');
+                return;
+            }
+        addStudentInTable(personToBeAdded);
+
+    });
+
+    function extractInputValues() {
+        const firstName = extractInputValueByName('first-name');
+        const lastName = extractInputValueByName('last-name');
+        const fn = extractInputValueByName('fn');
+        const mark = extractInputValueByName('mark');
+
+        return {
+            firstName,
+            lastName,
+            fn,
+            mark
+        }
+    }
+
+    function clearInputForm() {
+        const inputs = document.getElementsByTagName('input');
+        for (const input of inputs) {
+            input.value = '';
+        }
+    }
+
+    function extractInputValueByName(name) {
+        return document.getElementsByName(name)[0].value;
+    }
+
+    function addStudentInTable(student) {
+        const tBodyElement = document.querySelector('tbody');
+        const lastLine = tBodyElement.lastChild;
+        const tr = document.createElement('tr');
+        tr.setAttribute('class', 'student');
+        const firstNameTd = document.createElement('td');
+        firstNameTd.innerText = student.firstName;
+        const lastNameTd = document.createElement('td');
+        lastNameTd.innerText = student.lastName;
+
+        const fnTd = document.createElement('td');
+        fnTd.innerText = student.fn;
+        
+        const markTd = document.createElement('td');
+        markTd.innerText = student.mark;
+
+        const buttonTd = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.addEventListener('click', event => {
+            event.preventDefault();
+            event.stopPropagation();
+            deleteTableRow(event.currentTarget);
+        });
+        buttonTd.appendChild(deleteButton);
+        if (lastLine) {
+            lastLine.before(tr);
+        }
+        else {
+            tBodyElement.appendChild(tr);
+        }
+        tr.append(firstNameTd, lastNameTd, fnTd, markTd, buttonTd);
+        clearInputForm();
+    }
+
+
 })();
